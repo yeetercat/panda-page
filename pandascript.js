c92895b8ca3ebf3a7d183527bee75cacd3300387
yeetercat <114342414+yeetercat@users.noreply.github.com>
2026-03-23 13:53:24 +0100

    Initial commit

---
+++ pandascript.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+
+    hamburger.addEventListener("click", function () {
+        menu.classList.toggle("active");
+    });
+});
